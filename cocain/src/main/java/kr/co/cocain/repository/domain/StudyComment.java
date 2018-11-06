309b42f6457348ef9d9c87c98eb1b68e8de529be
loj3164 <42158879+loj3164@users.noreply.github.com>
2018-11-06 15:03:05 +0900

    11월 6일 댓글 조회수 누락부분 java파일 재 업로드

---
+++ cocain/src/main/java/kr/co/cocain/repository/domain/StudyComment.java
@@ -0,0 +1,42 @@
+package kr.co.cocain.repository.domain;
+
+import java.util.Date;
+
+public class StudyComment {
+	private int no;
+	private String id;
+	private String content;
+	private Date regDate;
+	private int commentNo;
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	public int getCommentNo() {
+		return commentNo;
+	}
+	public void setCommentNo(int commentNo) {
+		this.commentNo = commentNo;
+	}
+}
